77853d7bd6a21a23c1f24bf320a591b5f0929bcb
Claudeilton Dantas <fcd007@hotmail.com>
2021-02-10 17:09:18 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
+import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from '@/validation/validators/email'
 
 describe('EmailValidation', () => {
   test('should return error emai is invalid', async () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = new EmailValidation(faker.internet.email())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('should return falsy if email is invalid', async () => {
+    const sut = new EmailValidation(faker.internet.email())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
