e85da12e5b93a60e907acce55811b7a2b987f51e
Claudeilton Dantas <fcd007@hotmail.com>
2021-02-10 15:06:39 -0300

    test: ensure required field validation returns falsy if field not empty

--- src/validation/required-field-validation.spec.ts
+++ src/validation/required-field-validation.spec.ts
@@ -1,3 +1,4 @@
+import faker from 'faker'
 import { RequiredFieldValidation } from '@/validation'
 import { RequiredFieldError } from '@/validation/errors'
 
@@ -6,5 +7,11 @@ describe('', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('should return false if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
