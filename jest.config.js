1c04c483fb784235097b9762bf8a026e91c5d3a8
Claudeilton Dantas <fcd007@hotmail.com>
2021-02-23 16:22:47 -0300

    fix: ensure core dependencies jest errors tests

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,12 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/presentation/components/router/**/*',
     '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    '<rootDir>/src/main/test/cypress'
+    '<rootDir>/node_modules/'
   ],
   testEnvironment: 'jsdom',
   transform: {
